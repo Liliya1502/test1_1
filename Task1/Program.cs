a96f3e0347aee929fdb4f5568f187efb1e43a5d8
Epp <eppliliya@mail.ru>
2024-04-10 23:59:04 +0300

    add some variables lesson1:task1

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,13 +1,6 @@
 ﻿
 
-// Console.WriteLine ("Hello world");
+Console.WriteLine ("Hello world");
 
 int value = 100;
 Console.WriteLine(value);
-
-double d = 5.55;
-char c = '!';
-bool b = true;
-Console.WriteLine(d);
-Console.WriteLine(c);
-Console.WriteLine(b);
